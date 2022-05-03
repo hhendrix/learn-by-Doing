151edfba2b789f7419a349f3cdad0cebdf2c7175
Harry Lopez <harry.lopez@overactive.com>
2022-05-02 21:14:45 -0500

    Initial Commit

---
+++ learn by Doing/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  learn by Doing
+//
+//  Created by Harry Lopez on 2/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ learn by Doing/learn_by_DoingApp.swift
@@ -0,0 +1,17 @@
+//
+//  learn_by_DoingApp.swift
+//  learn by Doing
+//
+//  Created by Harry Lopez on 2/05/22.
+//
+
+import SwiftUI
+
+@main
+struct learn_by_DoingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
